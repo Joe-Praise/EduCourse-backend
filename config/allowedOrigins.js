f82ea958fe70f7517ecf79b04ddc85829c2ab6ec
Joe Praise <alimipraisejoe@gmail.com>
2024-02-21 23:20:06 +0100

    CORS allowed Origin access update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://edu-course-5u697u81n-joe-praise.vercel.app/',
+];
 
 module.exports = allowedOrigins;
