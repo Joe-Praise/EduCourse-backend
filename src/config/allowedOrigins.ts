d7e824a62493de8b03ee954283bf365d3a09b84a
Joe Praise <praisejosephalimi@gmail.com>
2026-06-13 22:35:24 +0100

    feat: add new origin to allowedOrigins for CORS configuration

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -2,6 +2,7 @@ const allowedOrigins = [
   'http://localhost:5174',
   'https://edu-course.vercel.app',
   'https://educourselearning.netlify.app',
+  'https://educourse.praisealimi.com/',
 ];
 
 export default allowedOrigins;
